2dfb2694c2e932e833c37bf7de0887a01d16e3b7
Marco Faletra <m.faletra@gmail.com>
2023-12-22 18:38:07 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,131 @@
+use std::fs::read_to_string;
+
+fn check1(lines: &str) -> u32
+{
+    let results = lines.split('\n');
+    let mut sum = 0;
+    for line in results {
+        let t: String = line.chars().filter(|c| c.is_ascii_digit()).collect();
+        if let Some(g) = t.chars().next()
+        {
+            sum += g.to_digit(10).unwrap() * 10;
+        }
+        if let Some(f)= t.chars().last() 
+        {
+            sum += f.to_digit(10).unwrap();
+        }
+    }
+    sum
+}
+
+
+fn check2(lines: &str) -> u32
+{
+    let vec_num = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let results = lines.split('\n');
+    let mut sum = 0;
+    //let mut riga = 0;
+    for line in results {
+        let mut value_digit: u32 = 0;
+        let mut digit_pos_founded = usize::MAX;
+        if let Some(digit_pos) = line.find(|c: char| c.is_ascii_digit())
+        {
+            digit_pos_founded = digit_pos;
+            if let Some(value) = line.chars().nth(digit_pos)
+            {
+                value_digit = value.to_digit(10).unwrap();
+            }
+        }
+
+        if digit_pos_founded != 0
+        {
+            let mut occurence = 1;
+            for number in &vec_num 
+            {
+                if let Some(index) = line.find(number)
+                {
+                    if index < digit_pos_founded
+                    {
+                        digit_pos_founded = index;
+                        value_digit = occurence;
+                    }
+                }
+                occurence += 1;
+            }
+        }
+
+        //let first_number = value_digit;
+        sum += value_digit * 10;
+
+
+        let mut digit_pos_founded = 0;
+        if let Some(digit_pos) = line.rfind(|c: char| c.is_ascii_digit())
+        {
+            digit_pos_founded = digit_pos;
+            if let Some(value) = line.chars().nth(digit_pos)
+            {
+                value_digit = value.to_digit(10).unwrap();
+            }
+        }
+
+        if digit_pos_founded != line.len() - 1
+        {
+            let mut occurence = 1;
+            for number in &vec_num 
+            {
+                if let Some(index) = line.rfind(number)
+                {
+                    if index > digit_pos_founded
+                    {
+                        digit_pos_founded = index;
+                        value_digit = occurence;
+                    }
+                }
+                occurence += 1;
+            }
+        }
+        sum += value_digit;
+
+        //println!("Riga {} {} {}", riga, first_number, value_digit);
+        //riga += 1;
+    }
+    sum
+}
+
+fn main() {
+    let lines = read_to_string("/home/marco/rust/AdventOfCode2023/day1/src/text1.txt").unwrap();
+    println!("{}",check1(&lines));
+
+    let lines = read_to_string("/home/marco/rust/AdventOfCode2023/day1/src/text1.txt").unwrap();
+    println!("{}",check2(&lines));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let test = "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet";
+
+        let result = check1(test);
+        assert_eq!(result, 142);
+    }
+
+    #[test]
+    fn test2() {
+        let test = "two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen";
+
+        let result = check2(test);
+        assert_eq!(result, 281);
+    }
+}
