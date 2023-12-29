085c98ff4fa5d83a681267dec4f985be0c6516da
Marco Faletra <m.faletra@gmail.com>
2023-12-29 16:57:43 +0100

    Done Day-06

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,4 @@
+use nom::character::complete::digit1;
 use nom::multi::separated_list1;
 use nom::sequence::separated_pair;
 use nom::character::complete::line_ending;
@@ -7,6 +8,7 @@ use nom::bytes::complete::is_not;
 use nom::Parser;
 use nom_supreme::parser_ext::ParserExt;
 use nom::character::complete;
+
 
 fn nums(input: &str) -> IResult<&str, Vec<u32>> {
     is_not("1234567890")
@@ -18,15 +20,47 @@ fn parse_times(input: &str) -> IResult<&str, (Vec<u32>, Vec<u32>)> {
     separated_pair(nums, line_ending, nums).parse(input)
 }
  
-fn part1(input: &str) -> u32 {
+fn part1(input: &str) -> usize {
     let (_, (times, distances)) = parse_times(input).expect("Error parsing");
-    dbg!(times, distances);
-    0
+    times
+        .into_iter()
+        .zip(distances)
+        .map(|(time, distance)| {
+            (1..time)
+                .into_iter()
+                .filter_map(|speed| {
+                    let my_distance = (time - speed) * speed;
+                    (my_distance > distance).then_some(my_distance)
+                })
+                .count()
+        })
+        .product::<usize>()
 }
 
-fn part2(input: &str) -> u64 {
-    let result = 0;
-    result
+fn nums_part2(input: &str) -> IResult<&str, usize> {
+    is_not("1234567890")
+        .precedes(separated_list1(space1, digit1)
+            .map(|x| { 
+                x.join("")
+                .parse::<usize>()
+                .expect("Expected a valid number")
+        }))
+        .parse(input)
+}
+
+fn parse_times_part2(input: &str) -> IResult<&str, (usize, usize)> {
+    separated_pair(nums_part2, line_ending, nums_part2).parse(input)
+}
+
+fn part2(input: &str) -> usize {
+    let (_, (time, distance)) = parse_times_part2(input).expect("Error parsing");
+    (1..time)
+        .into_iter()
+        .filter_map(|speed| {
+            let my_distance = (time - speed) * speed;
+            (my_distance > distance).then_some(my_distance)
+        })
+        .count()        
 }
 
 fn main() {
@@ -43,16 +77,17 @@ mod tests {
     #[test]
     fn test1() {
         let input = "Time:      7  15   30
-                     Distance:  9  40  200";
+Distance:  9  40  200";
         assert_eq!(288, part1(input));
     }
 
     #[test]
     fn test2() {
-        let result = part2("");
-        assert_eq!(result, 0);
+        let result = part2("Time:      71530
+Distance:  940200");
+        assert_eq!(result, 71503);
     }
-
+/* 
     fn my_is_not_test_1() {
         dbg!(is_not::<_, _, Error<_>>("1234567890").parse("Time: 7 15 30"));
         assert!(false);
@@ -67,4 +102,5 @@ mod tests {
         dbg!(tag::<_, _, Error<_>>("Time: ").precedes(separated_list1(space1, complete::u32)).parse("Time: 7 15 30"));
         assert!(false);
     }
+    */
 }
